6eb224c1026f48ee2201903922a9388982e60050
Muhammad hamza <102409025+muhammadhamza890@users.noreply.github.com>
2022-05-10 18:49:40 +0500

    Cpp first programe

---
+++ Hamza.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int num;
+    cout <<"Hello World"<<endl;
+    cout <<"Enter Number: ";
+    cin >>num;
+    cout <<"The Number you have entered is: " <<num;
+    return 0;
